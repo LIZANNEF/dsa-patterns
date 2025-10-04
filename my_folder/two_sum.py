5b5738f06212549fd32ea1358578a2abacadad29
ldfernandes <51730935+LIZANNEF@users.noreply.github.com>
2025-10-04 10:53:12 +0530

    Add files via upload

---
+++ my_folder/two_sum.py
@@ -0,0 +1,27 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         seen = {}  # value -> index
+
+#         for i, num in enumerate(nums):
+#             complement = target - num
+#             if complement in seen:
+#                 return [seen[complement], i]
+#             seen[num] = i
+
+#Given an array of integers nums and an integer target,
+# return the indices of the two numbers such that they add up to target.
+from typing import List
+class Solutions:
+    def twoSum(self, nums : List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i,num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num]=i
+            
+a = Solutions()
+print(a.twoSum([1,2,3,4,2,4],4))
+
+
